28d32d04fdf4bb5d0166c70df31a470c65f00497
Ali Vanaki <66532404+alivanaki79@users.noreply.github.com>
2025-05-11 10:55:16 -0700

    Update main.py

--- main.py
+++ main.py
@@ -52,3 +52,6 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))  # PORT متغیر مخصوص Render هست
     uvicorn.run("main:app", host="0.0.0.0", port=port)
 
+@app.get("/")
+def root():
+    return {"status": "Bot is alive!"}
